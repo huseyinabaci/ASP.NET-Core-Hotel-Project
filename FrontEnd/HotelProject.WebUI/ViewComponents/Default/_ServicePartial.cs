488c300097d7b6432499cbe79b4f96ad2489119d
Hüseyin ABACI <huseyin.abaci@hotmail.com>
2023-07-30 17:58:57 +0300

    Update

--- FrontEnd/HotelProject.WebUI/ViewComponents/Default/_ServicePartial.cs
+++ FrontEnd/HotelProject.WebUI/ViewComponents/Default/_ServicePartial.cs
@@ -10,8 +10,21 @@ namespace HotelProject.WebUI.ViewComponents.Default
 {
     public class _ServicePartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _ServicePartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:21182/api/Service");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
